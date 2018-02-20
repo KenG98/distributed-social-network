e051bb474524c0c2206ec5b504697a12c3944ccd
wpine215 <wpine215@gmail.com>
2018-02-20 22:54:46 +0000

    updated data structure and added procedurally generated avatars

--- chat/public/app.js
+++ chat/public/app.js
@@ -5,7 +5,9 @@ new Vue({
         ws: null, // Our websocket
         newMsg: '', // Holds new messages to be sent to the server
         chatContent: '', // A running list of chat messages displayed on the screen
-        email: null, // Email address used for grabbing an avatar
+        // email: null, // Email address used for grabbing an avatar
+        timestamp: 'placeholder',
+        action: 'msg',
         username: null, // Our username
         joined: false // True if email and username have been filled in
     },
@@ -13,14 +15,15 @@ new Vue({
     created: function() {
         var self = this;
         this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+        // Add code to retrieve all previous messages here and display them
         this.ws.addEventListener('message', function(e) {
             var msg = JSON.parse(e.data);
             self.chatContent += '<div class="chip">'
-                    + '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
+                    + '<img src="http://api.adorable.io/avatar/32/' + msg.username + '">' // Avatar
                     + msg.username
                 + '</div>'
-                + emojione.toImage(msg.message) + '<br/>'; // Parse emojis
-
+                + emojione.toImage(msg.contents) + '<br/>'; // Parse emojis
+                // Uses https://github.com/adorableio/avatars-api for random avatar generation
             var element = document.getElementById('chat-messages');
             element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
         });
@@ -31,9 +34,10 @@ new Vue({
         if (this.newMsg != '') {
             this.ws.send(
                 JSON.stringify({
-                    email: this.email,
+                    timestamp: this.timestamp,
                     username: this.username,
-                    message: $('<p>').html(this.newMsg).text() // Strip out html
+                    action: this.action,
+                    contents: $('<p>').html(this.newMsg).text() // Strip out html
                 }
             ));
             this.newMsg = ''; // Reset newMsg
@@ -41,20 +45,12 @@ new Vue({
     },
 
     join: function () {
-    if (!this.email) {
-        Materialize.toast('You must enter an email', 2000);
-        return
-    }
     if (!this.username) {
         Materialize.toast('You must choose a username', 2000);
         return
     }
-    this.email = $('<p>').html(this.email).text();
     this.username = $('<p>').html(this.username).text();
     this.joined = true;
     },
-    gravatarURL: function(email) {
-        return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
-    }
   }
 });
